e0b5b720112b7893477dbba46a441895de352519
Martin Németh <martin@revolin.io>
2024-01-02 16:36:27 +0100

    Add basic elements

---
+++ src/components/NoteCard.tsx
@@ -0,0 +1,9 @@
+const NoteCard = () => {
+  return (
+    <div className="flex items-center justify-center p-4 m-4 w-fit h-fit rounded-lg bg-neutral-600 shadow shadow-black min-h-32 min-w-60">
+      <p>Content</p>
+    </div>
+  );
+};
+
+export default NoteCard;
